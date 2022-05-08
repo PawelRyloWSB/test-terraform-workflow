49ebfa3def3bfbe1a6c5e516862d324379a838a9
student <student@ubuntu-17.edu-wsb.local>
2022-05-08 11:44:18 +0200

    Add terraform

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "local_file" "foo" {
+  content = "print(${var.hello})"
+  filename = var.filename
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "hello" {
+  type = string
+  default = "'Hello World'"
+}
+
+variable "filename" {
+  type = string
+  default = "app.py"
+}
